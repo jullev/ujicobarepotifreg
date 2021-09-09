65fac742b2ff07eeaf362b5394f210c1d0c7812f
Faruk <farukmicros@outlook.com>
2021-09-09 13:50:58 +0700

    Prettify code and fix parameter name

--- Ujicoba.java
+++ Ujicoba.java
@@ -1,9 +1,8 @@
 public class Ujicoba {
-	public static void main(String args){
-	System.out.println("Hello Worlds");
-	Integer nilai = 10;
-	System.out.println("Permisi Pak :V");
-	System.out.println("Polije Sip");
+	public static void main(String[] args) {
+		System.out.println("Hello Worlds");
+		Integer nilai = 10;
+		System.out.println("Permisi Pak :V");
+		System.out.println("Polije Sip");
 	}
-
 }
